31656bb37f3239854ae5ba4b73b971972c650dbf
cjordansmith <cjordansmith@gmail.com>
2016-05-27 03:16:24 -0500

    initial commit for Learn About Me App

---
+++ app.js
@@ -0,0 +1,83 @@
+//  Required modules for LAM
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var flash = require("connect-flash");
+var passport = require("passport");
+var session = require("express-session");
+var express = require("express");
+var mongoose = require("mongoose");
+var path = require("path");
+
+var setUpPassport = require("./setuppassport");
+//  Puts all routes in abother file
+var routes = require('./routes');
+
+//  Create express app
+var app = express();
+
+var gracefulShutdown;
+var dbURI = 'mongodb://localhost/lam';
+
+//  Connect to MongoDB server in the 'test' database
+mongoose.connect(dbURI);
+setUpPassport();
+
+// CONNECTION EVENTS
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected');
+});
+
+// CAPTURE APP TERMINATION / RESTART EVENTS
+// To be called when process is restarted or terminated
+gracefulShutdown = function(msg, callback) {
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+};
+// For nodemon restarts
+process.once('SIGUSR2', function() {
+    gracefulShutdown('nodemon restart', function() {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+// For app termination
+process.on('SIGINT', function() {
+    gracefulShutdown('app termination', function() {
+        process.exit(0);
+    });
+});
+
+//  Set port for server to start on
+app.set('port', process.env.PORT || 3000);
+
+//  Tell Express where to look for views folder
+// Define view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//  Use Middleware's below
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+  secret: "LUp$Dg?,I#i&owP3=9su+OB%`JgL4muLF5YJ~{;t",
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(routes);
+
+//  Start server on port listed above
+app.listen(app.get('port'), function() {
+  console.log("Server started on port " + app.get('port'));
+});
---
+++ routes.js
@@ -0,0 +1,108 @@
+/*  Required modules  */
+var express = require("express");
+var passport = require("passport");
+
+var User = require("./models/user");
+var router = express.Router();
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    req.flash('info', "You must be logged in to see this page.");
+    res.redirect('/login');
+  }
+}
+
+/*  Sets useful variables for templates */
+router.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.errors = req.flash('error');
+  res.locals.infos = req.flash('info');
+  next();
+});
+
+/*  Query the 'users' collection, returning the newest users first  */
+router.get("/", function(req, res, next) {
+  User.find()
+  .sort({ createdAt: "descending" })
+  .exec(function(err, users) {
+    if (err) { return next(err); }
+    res.render("index", { users: users });
+  });
+});
+
+/*  LOGIN AND LOGOUT PAGES  */
+router.get('/login', function(req, res) {
+  res.render('login');
+});
+
+router.post('/login', passport.authenticate('login', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}));
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.render('/');
+});
+
+/*  SIGNUP PAGES AND AUTHENTICATION */
+router.get("/signup", function(req, res) {
+  res.render("signup");
+});
+
+router.post("/signup", function(req, res, next) {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  User.findOne({ username: username }, function(err, user) {
+
+    if (err) { return next(err); }
+    if (user) {
+      req.flash("error", "User already exists");
+      return res.redirect("/signup");
+    }
+
+    var newUser = new User({
+      username: username,
+      password: password
+    });
+    newUser.save(next);//  Save the new user to the database and continue to the next request handler
+
+  });
+}, passport.authenticate("login", {
+  successRedirect: "/",
+  failureRedirect: "/signup",
+  failureFlash: true
+})
+);
+
+/*  EDIT PAGES */
+router.get('/users/:username', function(req, res, next) {
+  User.findOne({ username: req.params.username }, function(err, user) {
+    if (err) { return next(err); }
+    if (!user) { return next(404); }
+    res.render('profile', { user: user });
+  });
+});
+
+router.get('/edit', ensureAuthenticated, function(req, res) {
+  res.render('edit');
+});
+
+router.post('/edit', ensureAuthenticated, function(req, res, next) {
+  req.user.displayName = req.body.displayName;
+  req.user.bio = req.body.bio;
+  req.user.save(function(err) {
+    if (err) {
+      next(err);
+      return;
+    }
+    req.flash('info', "Profile updated!");
+    res.redirect('/edit');
+  });
+});
+
+module.exports = router;//  export the router module to rest of app
